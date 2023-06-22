4166a2abfa177e4544db2fe406843fa7067b0731
oxou <102489121+oxou@users.noreply.github.com>
2023-06-22 05:38:14 +0200

    Require root on installation

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo echo "#!/usr/bin/env php" >> /usr/bin/debsearch
-sudo cat cli.php >> /usr/bin/debsearch
+if [ $EUID != 0 ]; then
+    echo "You must be root to use this script."
+    exit 1
+fi
+
+echo "#!/usr/bin/env php" >> /usr/bin/debsearch
+cat cli.php >> /usr/bin/debsearch
 chmod +x /usr/bin/debsearch
 
 echo "debsearch installed."
